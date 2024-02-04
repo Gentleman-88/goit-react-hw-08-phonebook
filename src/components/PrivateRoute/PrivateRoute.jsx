76b34a4d59236e829eb5a0dd167fb8bdf4e0aa7a
Mark <tka4mark@gmail.com>
2024-02-04 23:24:45 +0200

    Update PrivateRoute.jsx

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,12 +1,16 @@
-import { selectUserIsLoggedIn } from '../../Redux/auth/AuthSlice.selectors';
+import {
+  selectUserIsLoggedIn,
+  selectUserIsRefreshing,
+} from '../../Redux/auth/AuthSlice.selectors';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
 const PrivateRoute = ({ children }) => {
   const isLoggedIn = useSelector(selectUserIsLoggedIn);
+  const isRefreshing = useSelector(selectUserIsRefreshing);
 
-  return !isLoggedIn ? <Navigate to="/" replace /> : children;
+  return !isLoggedIn && !isRefreshing ? <Navigate to="/" replace /> : children;
 };
 
 export default PrivateRoute;
